ecb251229c6a68adb5e811ebd0c44ac44a5362b2
David Miller <dmiller2718@gmail.com>
2013-04-02 22:32:56 -0500

    Initial translation of transport code. Loads.

--- src/clojure/tools/nrepl/transport.clj
+++ src/clojure/tools/nrepl/transport.clj
@@ -2,16 +2,18 @@
 (ns ^{:author "Chas Emerick"}
      clojure.tools.nrepl.transport
   (:require [clojure.tools.nrepl.bencode :as be]
-            [clojure.java.io :as io]
+            [clojure.clr.io :as io]                                       ;DM: clojure.java.io
+			[clojure.tools.nrepl.sync-channel :as sc]                     ;DM: Added
             (clojure walk set))
-  (:use [clojure.tools.nrepl.misc :only (returning uuid)])
+  (:use [clojure.tools.nrepl.misc :only (returning uuid)])  
   (:refer-clojure :exclude (send))
-  (:import (java.io InputStream OutputStream PushbackInputStream
-                    PushbackReader IOException EOFException)
-           (java.net Socket SocketException)
-           (java.util.concurrent SynchronousQueue LinkedBlockingQueue
-                                 BlockingQueue TimeUnit)
-           clojure.lang.RT))
+  (:import (System.IO  Stream  EndOfStreamException)                      ;DM: (java.io InputStream OutputStream PushbackInputStream
+           (clojure.lang PushbackInputStream PushbackTextReader)          ;DM:  PushbackReader IOException EOFException)
+           (System.Net.Sockets Socket SocketException)                    ;DM: (java.net Socket SocketException)
+           (System.Collections.Concurrent                                 ;DM: (java.util.concurrent SynchronousQueue LinkedBlockingQueue
+               |BlockingCollection`1[System.Object]|)                     ;DM: BlockingQueue TimeUnit)
+;		   clojure.tools.nrepl.transport.Transport                        ;DM: Added
+           clojure.lang.RT ))
 
 (defprotocol Transport
   "Defines the interface for a wire protocol implementation for use
@@ -26,28 +28,28 @@
   Transport
   ;; TODO this keywordization/stringification has no business being in FnTransport
   (send [this msg] (-> msg clojure.walk/stringify-keys send-fn) this)
-  (recv [this] (.recv this Long/MAX_VALUE))
+  (recv [this] (.recv this Int64/MaxValue))                                      ;DM: Long/MAX_VALUE
   (recv [this timeout] (clojure.walk/keywordize-keys (recv-fn timeout)))
-  java.io.Closeable
-  (close [this] (close)))
+  System.IDisposable                                                             ;DM: java.io.Closeable
+  (Dispose [this] (close)))                                                      ;DM: (close [this] (close)))  TODO: This violates good IDisposable practice
 
 (defn fn-transport
   "Returns a Transport implementation that delegates its functionality
    to the 2 or 3 functions provided."
   ([read write] (fn-transport read write nil))
   ([read write close]
-    (let [read-queue (SynchronousQueue.)]
+    (let [read-queue (sc/make-simple-sync-channel)]                               ;DM: (SynchronousQueue.)
       (future (try
                 (while true
-                  (.put read-queue (read)))
-                (catch Throwable t
-                  (.put read-queue t))))
+                  (sc/put read-queue (read)))                                     ;DM: .put
+                (catch Exception t                                                ;DM: Throwable
+                  (sc/put read-queue t))))                                        ;DM: .put
       (FnTransport.
         (let [failure (atom nil)]
           #(if @failure
              (throw @failure)
-             (let [msg (.poll read-queue % TimeUnit/MILLISECONDS)]
-               (if (instance? Throwable msg)
+             (let [msg (sc/poll read-queue % )]                                   ;DM:  .poll, remove TimeUnit/MILLISECONDS
+               (if (instance? Exception msg)                                      ;DM: Throwable
                  (do (reset! failure msg) (throw msg))
                  msg))))
         write
@@ -58,10 +60,12 @@
 (defmethod <bytes :default
   [input]
   input)
-
-(defmethod <bytes (RT/classForName "[B")
-  [#^"[B" input]
-  (String. input "UTF-8"))
+                                                                           ;DM:Added
+(def #^{:private true} utf8 (System.Text.UTF8Encoding.))          ;DM:Added
+                                                                           ;DM:Added
+(defmethod <bytes |System.Byte[]|                                          ;DM: (RT/classForName "[B")
+  [#^|System.Byte[]| input]                                                ;DM: #^"[B"
+  (.GetString utf8 input))                                                 ;DM: (String. input "UTF-8"))
 
 (defmethod <bytes clojure.lang.IPersistentVector
   [input]
@@ -77,10 +81,10 @@
   [^Socket s & body]
   `(try
      ~@body
-     (catch EOFException e#
+     (catch EndOfStreamException e#                                        ;DM: EOFException
        (throw (SocketException. "The transport's socket appears to have lost its connection to the nREPL server")))
-     (catch Throwable e#
-       (if (and ~s (not (.isConnected ~s)))
+     (catch Exception e#                                                   ;DM: Throwable
+       (if (and ~s (not (.Connected ~s)))                                  ;DM: .isConnected
          (throw (SocketException. "The transport's socket appears to have lost its connection to the nREPL server"))
          (throw e#)))))
 
@@ -102,23 +106,23 @@
            (locking out
              (doto out
                (be/write-bencode %)
-               .flush)))
+               .Flush)))                                                  ;DM: .flush
         (fn []
-          (.close in)
-          (.close out)
-          (when s (.close s)))))))
+          (.Close in)                                                     ;DM: .close
+          (.Close out)                                                    ;DM: .close
+          (when s (.Close s)))))))                                        ;DM: .close
 
 (defn tty
   "Returns a Transport implementation suitable for serving an nREPL backend
    via simple in/out readers, as with a tty or telnet connection."
   ([^Socket s] (tty s s s))
   ([in out & [^Socket s]]
-    (let [r (PushbackReader. (io/reader in))
-          w (io/writer out)
+    (let [r (PushbackTextReader. (io/text-reader in))                     ;DM: PushbackReader. io/reader
+          w (io/text-writer out)                                          ;DM: io/writer
           cns (atom "user")
           prompt (fn [newline?]
-                   (when newline? (.write w (int \newline)))
-                   (.write w (str @cns "=> ")))
+                   (when newline? (.Write w (int \newline)))              ;DM: .write
+                   (.Write w (str @cns "=> ")))                           ;DM: .write
           session-id (atom nil)
           read-msg #(let [code (read r)]
                       (merge {:op "eval" :code [code] :ns @cns :id (str "eval" (uuid))}
@@ -128,10 +132,10 @@
                   (when new-session (reset! session-id new-session))
                   (when ns (reset! cns ns))
                   (doseq [^String x [out err value] :when x]
-                    (.write w x))
+                    (.Write w x))                                                    ;DM: .write
                   (when (and (= status #{:done}) id (.startsWith ^String id "eval"))
                     (prompt true))
-                  (.flush w))
+                  (.Flush w))                                                        ;DM: .flush
           read #(let [head (promise)]
                   (swap! read-seq (fn [s]
                                      (deliver head (first s))
@@ -140,7 +144,7 @@
       (fn-transport read write
         (when s
           (swap! read-seq (partial cons {:session @session-id :op "close"}))
-          #(.close s))))))
+          #(.Close s))))))                                                         ;DM: .close
 
 (defn tty-greeting
   "A greeting fn usable with clojure.tools.nrepl.server/start-server,
@@ -153,15 +157,16 @@
   (send transport {:out (str ";; Clojure " (clojure-version)
                              \newline "user=> ")}))
 
-(deftype QueueTransport [^BlockingQueue in ^BlockingQueue out]
+(deftype QueueTransport [^|System.Collections.Concurrent.BlockingCollection`1[System.Object]| in 
+                         ^|System.Collections.Concurrent.BlockingCollection`1[System.Object]| out]           ;DM: ^BlockingQueue
   clojure.tools.nrepl.transport.Transport
-  (send [this msg] (.put out msg) this)
-  (recv [this] (.take in))
-  (recv [this timeout] (.poll in timeout TimeUnit/MILLISECONDS)))
+  (send [this msg] (.Add out msg) this)                                            ;DM: .put
+  (recv [this] (.Take in))                                                         ;DM: .take
+  (recv [this timeout] (let [x nil] (.TryTake in (ref x) (int timeout)) x)))      ;DM: .poll, removed TimeUnit/MILLISECONDS, added (int .), let, ref
 
 (defn piped-transports
   "Returns a pair of Transports that read from and write to each other."
   []
-  (let [a (LinkedBlockingQueue.)
-        b (LinkedBlockingQueue.)]
+  (let [a (|System.Collections.Concurrent.BlockingCollection`1[System.Object]|.)                                                    ;DM: LinkedBlockingQueue      
+        b (|System.Collections.Concurrent.BlockingCollection`1[System.Object]|.)]                                                   ;DM: LinkedBlockingQueue  
     [(QueueTransport. a b) (QueueTransport. b a)]))
