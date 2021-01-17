7930d52da1845932d02202c23402bd6c9aaae8d8
David Miller <dmiller2718@gmail.com>
2021-01-17 10:24:06 -0600

    Let's really start over.

--- test/clojure/nrepl/misc_test.clj
+++
@@ -1,9 +0,0 @@
-(ns nrepl.misc-test
-  (:require [clojure.test :refer [deftest is]]
-            [nrepl.misc :as misc]))
-
-(deftest sanitize-meta-test
-  (is (not-empty (:file (misc/sanitize-meta {:file "clojure/core.clj"}))))
-
-  (is (= "/foo/bar/baz.clj"
-         (:file (misc/sanitize-meta {:file "/foo/bar/baz.clj"})))))
